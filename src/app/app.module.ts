bffd2baf3b8203f461b90ff6aee6510adf3759fc
jan lawrence <56373608+janhawk@users.noreply.github.com>
2020-11-02 17:24:18 +0100

    my-cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './cvTech/cv/cv.component';
+import { ListeCvComponent } from './cvTech/liste-cv/liste-cv.component';
+import { ItemCvComponent } from './cvTech/item-cv/item-cv.component';
+import { DetailCvComponent } from './cvTech/detail-cv/detail-cv.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ListeCvComponent,
+    ItemCvComponent,
+    DetailCvComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
