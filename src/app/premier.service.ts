326b1810a19feaff3a6ed17efe8aa976cd28b89f
jan lawrence <56373608+janhawk@users.noreply.github.com>
2020-11-16 16:17:23 +0100

    my-cv

--- src/app/premier.service.ts
+++ src/app/premier.service.ts
@@ -17,11 +17,11 @@ export class PremierService {
   }
 }
 
-// Un service est un médiateur entre la vue et la logique
-// Ce qui n’est pas trivial doit être écrit sous forme d’un composant
-// Un service est associé à un composant en utilisant l’injection de dépendance
+// Un service est un médiateur entre la vue et la logique.
+// Ce qui n’est pas trivial doit être écrit sous forme d’un composant.
+// Un service est associé à un composant en utilisant l’injection de dépendance.
 // L’injection de dépendance utilise les étapes suivantes :
-// Déclarer le service dans le provider du module ou du composant
+// Déclarer le service dans le provider du module ou du composant.
 // Passer le service comme paramètre du constructeur de l’entité qui en a besoin.
 
 // un service est une classe  qui va contenir du code metier qui va servir a l'ensemble.
